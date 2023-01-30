bf2b8eaa97149e9d38fa8144ae535863c5c189ff
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-01-31 02:46:09 +0300

    Completed Lab

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,15 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+
 console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} age={user.age} city={user.city} color={user.color}/>
+      <About bio={user.bio} links={user.links} github={user.links.github}  linkedin={user.links.linkedin}/>
     </div>
   );
 }
